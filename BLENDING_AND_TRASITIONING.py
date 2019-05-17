66582c7ef26db0799b34b5b958253c4613feb7bf
manangupta12 <43471147+manangupta12@users.noreply.github.com>
2019-05-17 19:16:01 +0530

    Update BLENDING_AND_TRASITIONING.py

--- BLENDING_AND_TRASITIONING.py
+++ BLENDING_AND_TRASITIONING.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  7 12:54:56 2019
-
-@author: lappy
-"""
-
 import cv2
 import numpy as np
 import time
 
-path="C:\\Users\\lappy\\Desktop\\image dataset\\misc\\"
-imgpath1=path + "4.2.01.tiff"
-imgpath2=path + "4.2.07.tiff"
+imgpath1="4.2.01.tiff"
+imgpath2="4.2.07.tiff"
 
 img1=cv2.imread(imgpath1,1)
 img2=cv2.imread(imgpath2,1)
